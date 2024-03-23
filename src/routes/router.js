3c5628ca83cb0de6ade5823af08a054ea12211c9
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2024-03-23 18:54:55 +0800

    file outputting yay!

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,9 +2,23 @@ const { Router }= require('express');
 const { Decimal } = require('decimal.js');
 const {getExponentPrime, getCombiField, fifteenDPB, toHex} = require("../../public/js/helper.js");
 const router = Router();
+const fs = require('fs');
 
 router.get("/", (req, res) => {
     res.sendFile(__dirname + "\\" + 'index.html');
+});
+
+router.post ("/output", (req, res) => {
+    let num = decodeURIComponent(req.query.num);
+    let bin = decodeURIComponent(req.query.bin);
+    let hex = req.query.hex;
+    const content = `Read number: ${num}\nDecimal-64 in binary: ${bin}\nDecimal-64 in hex: ${hex}\n`;
+    console.log(__dirname);
+    fs.writeFile(__dirname+"/output.txt", content, { flag: 'a+' }, e => {
+        if (e) {
+            console.log(e);
+        }
+    })
 });
 
 router.post("/compute", (req, res) => {
@@ -12,14 +26,14 @@ router.post("/compute", (req, res) => {
     let rounding = req.query.rounding;
 
     if (num.match(/^-0+(\.0*)?(e(\+|-)?[0-9]+)?$/)) {
-        let s = "0b10000" + "0".repeat(59);
+        let s = "0b 1 00000 00000000" + " 0000000000".repeat(5);
         res.json({
             num: "-0",
             bin: s,
             hex: "0x8000000000000000"
         });
     } else if (num.match(/^(\+)?0+(\.0*)?(e(\+|-)?[0-9]+)?$/)) {
-        let s = "0b00000" + "0".repeat(59);
+        let s = "0b 0 00000 00000000" + " 0000000000".repeat(5);
         res.json({
             num: "0",
             bin: s,
@@ -36,14 +50,14 @@ router.post("/compute", (req, res) => {
         let min = new Decimal("-9999999999999999e+369");
 
         if (d.lessThan(min)) {
-            let s = "0b11111" + "0".repeat(59);
+            let s = "0b 1 11110 00000000" + " 0000000000".repeat(5)
             res.json({
                 num: "-Infinity",
                 bin: s,
                 hex: "0xF800000000000000"
             });
         } else if (d.greaterThan(max)) {
-            let s = "0b01111" + "0".repeat(59);
+            let s = "0b 0 11110 00000000" + " 0000000000".repeat(5)
             res.json({
                 num: "Infinity",
                 bin: s,
@@ -77,14 +91,14 @@ router.post("/compute", (req, res) => {
 
             if (exponentPrime < 0) {
                 if (isNegative) {
-                    let k = "0b10000" + "0".repeat(59);
+                    let k = "0b 1 00000 00000000" + " 0000000000".repeat(5)
                     res.json({
                         num: s,
                         bin: k,
                         hex: "0x8000000000000000"
                     });
                 } else {
-                    let k = "0b00000" + "0".repeat(59);
+                    let k = "0b 0 00000 00000000" + " 0000000000".repeat(5)
                     res.json({
                         num: s,
                         bin: k,
@@ -101,9 +115,9 @@ router.post("/compute", (req, res) => {
                 signBit = "1";
             }
             let contiField = parseInt(exponentPrime.toString()).toString(2).padStart(10, "0").slice(2);
-            let bin = signBit + combiField + contiField + bcdMantissa;
-            let hex = toHex(bin);
-            bin = "0b" + bin;
+            let bin = signBit +" "+ combiField + " "+ contiField +" "+ bcdMantissa.match(/.{1,10}/g).join(" ");
+            let hex = toHex(signBit + combiField + contiField + bcdMantissa);
+            bin = "0b " + bin;
             if (isNegative) {
                 s = "-" + s;
             }
@@ -114,7 +128,7 @@ router.post("/compute", (req, res) => {
             });
         }
     } else {
-        let s = "0b111111" + "0".repeat(58);
+        let s = "0b 1 11111 00000000" + " 0000000000".repeat(5)
         res.json({
             num: "NaN",
             bin: s,
@@ -122,5 +136,7 @@ router.post("/compute", (req, res) => {
         });
     }
 });
+
+
 
 module.exports = router;
